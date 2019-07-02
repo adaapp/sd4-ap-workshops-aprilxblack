9edc64c0e683539c6eb74cb6ac38c2a88663dedf
Karolina Zdziarska <Karolina.Zdziarska@britishland.com>
2019-07-02 11:59:42 +0100

    task 1 complete

---
+++ example-workshop-one/src/models/YearGroup.js
@@ -0,0 +1,16 @@
+class YearGroup {
+    constructor(year, headOfYear){
+        this._year = year;
+        this._headOfYear = headOfYear;
+    }
+
+    getYear(){
+        return this._year;
+    }
+
+    getHeadOfYear(){
+        return this._headOfYear;
+    }
+}
+
+module.exports = YearGroup;
